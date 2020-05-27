4a1bf7a1e27c43dbbad649aa9577e1656befe9c6
Richard Casanova <ricardofer2412@hotmail.com>
2020-05-27 14:48:43 -0400

    add components

---
+++ components/dashboard.js
@@ -0,0 +1,59 @@
+
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import firebase from '../database/firebase';
+import { Card } from 'react-native-material-ui';
+
+
+
+export default class Dashboard extends Component {
+  constructor() {
+    super();
+    this.state = { 
+      uid: ''
+    }
+  }
+
+  signOut = () => {
+    firebase.auth().signOut().then(() => {
+      this.props.navigation.navigate('Login')
+    })
+    .catch(error => this.setState({ errorMessage: error.message }))
+  }  
+
+  render() {
+    this.state = { 
+      displayName: firebase.auth().currentUser.displayName,
+      uid: firebase.auth().currentUser.uid
+    }    
+    return (
+      <View style={styles.container}>
+
+      <Card>
+        <Text>Hello world!</Text>
+      </Card>
+
+        <Button
+          color="#3740FE"
+          title="Logout"
+          onPress={() => this.signOut()}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: "flex",
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 35,
+    backgroundColor: '#fff'
+  },
+  textStyle: {
+    fontSize: 15,
+    marginBottom: 20
+  }
+});
---
+++ database/firebase.js
@@ -0,0 +1,17 @@
+
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBrNNecSwdCNc1qUQ2yy0j0O0nNHXi6oAg",
+    authDomain: "invoices-9d0b8.firebaseapp.com",
+    databaseURL: "https://invoices-9d0b8.firebaseio.com",
+    projectId: "invoices-9d0b8",
+    storageBucket: "invoices-9d0b8.appspot.com",
+    messagingSenderId: "421803080680",
+    appId: "1:421803080680:web:b82d64a7f999e43282025d",
+    measurementId: "G-01M4LRMBWR"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
